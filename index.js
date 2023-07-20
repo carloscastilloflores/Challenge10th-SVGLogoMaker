8768446f742ff0cc552f2d06e308ff4232aaa4c7
carloscastilloflores <125534814+carloscastilloflores@users.noreply.github.com>
2023-07-19 22:09:15 -0600

    functionality for taking user input and logic for svg creation, modified the shapes.js file

--- index.js
+++ index.js
@@ -3,33 +3,66 @@ import fs from "fs";
 
 import { Triangle, Square, Circle } from "./lib/shapes.js";
   
-function writeToFile(fileName, answers) {
-  let svgString = " ";
-  svgString = '<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">';
-  svgString += "<g>";
-  svgString += `${answers.shape}`;
+// function writeToFile(fileName, answers) {
+//   let svgString = " ";
+//   svgString = '<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">';
+//   svgString += "<g>";
+//   svgString += `${answers.shape}`;
 
-  let shapeChoice; 
+//   let shapeChoice;
+   
+//   if (answers.shape === "Triangle") {
+//     shapeChoice = new Triangle(); 
+//     svgString += `<polygon points="150, 18 244, 182 56, 182" fill="${answers.shapeBackground}"/>`;
+//   } else if (answers.shape === "Square") {
+//     shapeChoice = new Square(); 
+//     svgString += `<rect x="73" y="40" width="160" height="160" fill="${answers.shapeBackground}" />`;
+//   } else {
+//     shapeChoice = new Circle(); 
+//     svgString += `<circle cx="150" cy="115" r="80" fill="${answers.shapeBackground}" />`
+//   }
+
+//   svgString += `<text x="150" y="130" text-anchor="middle" font-size="40" fill="${answers.textColor}">${answers.text}</text>`;
+//   svgString += "</g>";
+//   svgString += "</svg>";
+  
+//   fs.writeFile(fileName, svgString, (err) => {
+//     err ? console.log(err) : console.log("Generated logo.svg");
+//   });
+// } 
+
+function writeToFile(fileName, answers) {
+  const shapeBackground = answers.shapeBackground;
+  const textColor = answers.textColor;
+  const text = answers.text;
+
+  let shapeChoice;
+
   if (answers.shape === "Triangle") {
-    shapeChoice = new Triangle(); 
-    svgString += `<polygon points="150, 18 244, 182 56, 182" fill="${answers.shapeBackground}"/>`;
+    shapeChoice = new Triangle();
   } else if (answers.shape === "Square") {
-    shapeChoice = new Square(); 
-    svgString += `<rect x="73" y="40" width="160" height="160" fill="${answers.shapeBackground}" />`;
+    shapeChoice = new Square();
   } else {
-    shapeChoice = new Circle(); 
-    svgString += `<circle cx="150" cy="115" r="80" fill="${answers.shapeBackground}" />`
+    shapeChoice = new Circle();
   }
 
-  svgString += `<text x="150" y="130" text-anchor="middle" font-size="40" fill="${answers.textColor}">${answers.text}</text>`;
-  svgString += "</g>";
-  svgString += "</svg>";
-  
-  fs.writeFile(fileName, svgString, (err) => {
-    err ? console.log(err) : console.log("Generated logo.svg");
-  });
-} 
+  const svgString = `
+    <svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+      <g>
+       ${shapeChoice.render(shapeBackground)}  
+        <text x="150" y="130" text-anchor="middle" font-size="40" fill="${textColor}">${text}</text>
+      </g>
+    </svg>
+  `;
 
+  fs.writeFile(fileName, svgString, (err) => {
+    if (err) {
+      console.error(err);
+    } else {
+      console.log("Generated logo.svg");
+    }
+  });
+}
 
 
 function mainPrompt() {
